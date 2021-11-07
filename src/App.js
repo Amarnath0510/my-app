983f7627cf3efbae34e7c87495420613ff8cb567
Amarnath0510 <amarmuthu007@gmail.com>
2021-11-07 08:54:37 +0530

    User list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  console.log("Hi");
+ const users=[
+   
+  {name: "amar",pic:"https://bestprofilepictures.com/wp-content/uploads/2021/04/Cool-Picture.jpg"},
+  {name: "vishwa",pic:"https://live.staticflickr.com/3893/14921394066_cb305327fe_b.jpg"},
+  {name: "prakash",pic:"https://www.teahub.io/photos/full/249-2492643_profile-dp-for-boys-whatsapp.jpg"},
+   {name:"venkat",pic:"https://www.photodoozy.com/uploads/cool-profile-pictures-stylish-dp-for-boys.jpg"},];
+  
+ 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="App">
+    
+    {users.map((ur)=> (
+      <Msg 
+      name={ur.name}
+      pic={ur.pic}
+      
+      />
+      ))}
     </div>
+
+   
   );
 }
-
-export default App;
+  function Msg({name,pic}){
+   
+  return(
+     <div>
+      <img className="user-pic" src={pic} alt="name"/>     
+       <h1 className="user-name">🙋‍♂️,{name}!!!😍</h1>
+       
+       </div>
+    );
+  }
+ 
